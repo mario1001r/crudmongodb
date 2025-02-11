0e3ee8569f6dcfbabc6f5c07da51925acafd8a39
mario650m <mario650m@gmail.com>
2025-02-11 17:27:00 -0600

    Add model and migrations Countries, States and Cities

---
+++ crudmongodb/app/Models/State.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\SoftDeletes;
+
+class State extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+	protected $connection = 'mongodb';
+
+    protected $collection = 'states'; 
+}
